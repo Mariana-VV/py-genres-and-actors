6764a0595b85913f66ffe170a143a823064ff2be
Mariana Trinko <marjanavv@gmail.com>
2026-02-16 19:42:07 +0100

    Solution

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import init_django_orm  # noqa: F401
 from db.models import Genre, Actor
 
 
-def main() -> QuerySet[Actor, Actor]:
+def main() -> QuerySet:
     genres = ["Western", "Action", "Dramma"]
     actors = [("George", "Klooney"), ("Kianu", "Reaves"),
               ("Scarlett", "Keegan"), ("Will", "Smith"),
